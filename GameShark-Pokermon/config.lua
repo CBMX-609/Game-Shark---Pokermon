27c8b0f68519823a0834fe68b6f6670ef03431f9
CBMX <Burnttoast0609@gmail.com>
2025-09-23 00:20:23 -0600

    CHORE - Delete GameShark-Pokermon directory

--- GameShark-Pokermon/config.lua
+++
@@ -1,7 +0,0 @@
-return {
-    gameSharkCustomJokers = true,
-    CeladonGameCorner = true,
-    DarkCave = true,
-    gamesharkPokeBallBackgroundColor = true,
-    gamesharkUltraBallBackgroundColor = false,
-}
